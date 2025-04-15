a3166f80c8920c0d39f053b0703b296dec291f4a
Johannes Schindelin <johannes.schindelin@gmx.de>
2025-04-15 12:25:33 +0200

    fixup??? survey: add command line opts to select references
    The `-ll` header went away again...
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- builtin/survey.c
+++ builtin/survey.c
@@ -6,7 +6,7 @@
 #include "hex.h"
 #include "object.h"
 #include "object-name.h"
-#include "object-store-ll.h"
+#include "object-store.h"
 #include "parse-options.h"
 #include "path-walk.h"
 #include "progress.h"
